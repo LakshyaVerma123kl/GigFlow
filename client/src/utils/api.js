eb05064e21078e36284475c5533f910e34691bc8
LakshyaVerma123kl <lakshya123kl@gmail.com>
2026-01-13 20:51:07 +0530

    fixes 01

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { store } from "../redux/store";
 
 const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
@@ -8,16 +7,19 @@ const api = axios.create({
   withCredentials: true,
 });
 
-// Add a request interceptor that reads token from Redux store on EVERY request
 api.interceptors.request.use(
   (config) => {
-    // Get fresh token from Redux store
-    const state = store.getState();
-    const user = state.auth.user;
+    const userStr = localStorage.getItem("user");
 
-    // Attach token if it exists
-    if (user && user.token) {
-      config.headers.Authorization = `Bearer ${user.token}`;
+    if (userStr) {
+      try {
+        const user = JSON.parse(userStr);
+        if (user && user.token) {
+          config.headers.Authorization = `Bearer ${user.token}`;
+        }
+      } catch (error) {
+        console.error("Error parsing user from localStorage:", error);
+      }
     }
 
     return config;
